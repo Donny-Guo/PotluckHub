109f102aaf64d76fa2cb044bc5d073282a37550f
Donny <107092000+Donny-Guo@users.noreply.github.com>
2026-04-21 15:34:12 -0400

    Start working on home page (WIP)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Home from "./pages/Home.jsx"
 
 function App() {
     return (
         <div>
-            helloodjf aoijdfas odifjasodifj aso;difj 
+            <Home />
         </div>
     )
 }
